efca559e52d39111b2a8badd25d0e775d4760cce
fuselierr <mikegao000@gmail.com>
2025-01-08 01:44:28 -0500

    integrated Typewriter module

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,10 +1,18 @@
+"use client"
+
+import Typewriter from 'typewriter-effect';
+
 const Home = () => {
   return (
     <section id="home" className="w-full flex-center flex-col">
         <h1 className="head_text text-center">
-            Hi, I'm
-            <br className="max-md:hidden" />
-            <span className="orange_gradient text-center">Mike Gao.</span>
+            <Typewriter
+                options = {{
+                    strings:['Hi, I\'m Mike Gao.'],
+                    autoStart: true,
+                    loop: true,
+                }}
+            />
         </h1>
     </section>
   )
