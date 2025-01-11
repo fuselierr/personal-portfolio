e1ada7e01ba65909682a066547ebe19ebe98e4bf
fuselierr <mikegao000@gmail.com>
2025-01-10 22:17:05 -0500

    fixed aos

--- app/page.jsx
+++ app/page.jsx
@@ -7,23 +7,26 @@ import Experience from '@components/Experience'
 import Projects from '@components/Projects'
 import Contact from '@components/Contact'
 import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 const Page = () => {
   useEffect(() => {
     AOS.init({
-      duration: 1500
+      duration: 1500,
     });
   })
   return (
     <section className="w-full flex-center flex-col">
-        <Home/>
-        <section id="about">
+        <section id="home" data-aos="fade-down" data-aos-mirror="true">
+          <Home/>
+        </section>
+        <section id="about" data-aos="fade-up" data-aos-duration="1500" data-aos-mirror="true">
           <About/>
         </section>
-        <section id="experience">
+        <section id="experience" data-aos="fade-up" data-aos-duration="1500" data-aos-mirror="true">
           <Experience/>
         </section>
-        <section id="projects">
+        <section id="projects" data-aos="fade-up" data-aos-duration="1500" data-aos-mirror="true">
           <Projects/>
         </section>
         <section id="contact" data-aos="fade-up" data-aos-duration="1500" data-aos-mirror="true">
